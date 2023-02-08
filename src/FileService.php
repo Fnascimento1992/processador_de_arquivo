43f9032ac7fb44bec2660364e3318251d149e523
Fernando Nascimento <ln330011@gmail.com>
2023-02-07 22:57:41 -0300

    v1.0.0

---
+++ src/FileService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Src;
+
+class FileService
+{    
+    /**
+     * @var mixed filename
+     */
+    private $filename;    
+    /**
+     * @var mixed sufix
+     */
+    private $sufix;
+
+
+        
+    /**
+     * splitFile
+     *
+     * @return void
+     */
+    public function splitFileofLine()
+    {
+        #code...
+        echo "Método de Split por Linhas"; 
+ 
+    }
+    
+    /**
+     * splitFileOfSize
+     *
+     * @return void
+     */
+    public function splitFileOfSize()
+    {
+        echo "Método de Split por tamanho"; 
+    }
+
+}
